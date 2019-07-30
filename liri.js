d50bebca34aa356fd98c76745cf1594da9ddc686
Yating(Esther) Su <yaringsu@foxmail.com>
2019-07-29 23:00:14 -0400

    Add movie search part

--- liri.js
+++ liri.js
@@ -24,3 +24,22 @@ console.log(userInput);
 //     console.log(data);
 // })
 
+if(userInput[0] === "movie-this"){
+    var movieName = userInput;
+    movieName.shift();
+    console.log(userInput.toString());
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName.toString() + "&y=&plot=short&apikey=trilogy";
+    console.log(queryUrl);
+    axios.get(queryUrl).then(
+        function(response){
+            
+            console.log("* The tiltle of the Movie : " + response.data.Title);
+            console.log("* The Released Year : " + response.data.Year);
+            console.log("* The Discription : " + response.data.Plot);
+            console.log("* IMDB Rating of the Movie : " + response.data.imdbRating);  
+            console.log("* Rotten Tomatoes Rating of the moive : " + response.data.Ratings[1].Value);
+            console.log("* Country where the movie was produced : " + response.data.Country);
+            console.log("* Language of the movie : " + response.data.Language);
+            console.log("* Actors in the movie : " + response.data.Actors);
+        });
+}
